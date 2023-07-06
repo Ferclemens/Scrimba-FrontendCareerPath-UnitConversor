4742dc391470460dd052788c38c897d9e53dd3c1
Ferclemens <69605971+Ferclemens@users.noreply.github.com>
2023-07-06 11:31:25 -0300

    functional unit converter app

---
+++ index.js
@@ -0,0 +1,62 @@
+
+//references
+const inputEl = document.getElementById('input-el')
+const btnEl = document.getElementById('btn-el')
+const conversionEl = document.getElementById('conversion-box')
+
+//listeners
+btnEl.addEventListener('click',function() {
+    const units = inputEl.value
+    render(units)
+})
+
+//functions
+/* 1 meter = 3.281 feet | 1 liter = 0.264 gallon | 1 kilogram = 2.204 pound */
+
+const unit = [3.281, 0.264, 2.204]
+
+function render(number) {
+    const results = unitsConversion(number)
+    conversionEl.innerHTML = results
+}
+
+function unitsConversion(units) {
+    let results = [] 
+    let divisionResult = null
+    let multiplyResult = null
+
+    for(let i = 0; i < unit.length; i++) {
+        if(i === 0) {
+            divisionResult = (units / unit[i]).toFixed(3)
+            multiplyResult = (units * unit[i]).toFixed(3)
+            results += `
+                <div class="conversion-description">
+                    <h2>Length (Meter/Feet)</h2>
+                    <p>${units} meters = ${multiplyResult} feet | ${units} feet = ${divisionResult} meters</p>
+                </div>
+            `
+        } else if(i === 1) {
+            divisionResult = (units / unit[i]).toFixed(3)
+            multiplyResult = (units * unit[i]).toFixed(3)
+            results += `
+                <div class="conversion-description">
+                    <h2>Volume (Liters/Gallons)</h2>
+                    <p>${units} liters = ${multiplyResult} gallons | ${units} gallons = ${divisionResult} liters</p>
+                </div>
+            `
+        } else if(i === 2) {
+            divisionResult = (units / unit[i]).toFixed(3)
+            multiplyResult = (units * unit[i]).toFixed(3)
+            results += `
+                <div class="conversion-description">
+                    <h2>Mass (Kilograms/Pounds)</h2>
+                    <p>${units} kilos = ${multiplyResult} pounds | ${units} pounds = ${divisionResult} kilos</p>
+                </div>
+            `
+        }
+
+    }  
+    console.log(results);
+    return results
+    
+}
